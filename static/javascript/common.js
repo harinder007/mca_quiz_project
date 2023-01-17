f72197ac0c4c2f22eea499a597dc4942ff16113f
vikashkumar9 <vikashparjapati59@gmail.com>
2023-01-17 11:14:45 +0530

    changes to deploy on netlify

--- static/javascript/common.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('item1').addEventListener('click', ()=> {
-    location.href="https://quiz-project-harinder.herokuapp.com";
-    console.log('hatoikojk');
-    localStorage.clear();
-});
-document.getElementById('item3').addEventListener('click', ()=> {
-    location.href="https://quiz-project-harinder.herokuapp.com/about";
-    console.log('hatoikojk')
-})
