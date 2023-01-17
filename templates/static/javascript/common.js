bfeebb36a72aa8807b14b05c2dd7861aad234f6b
vikashkumar9 <vikashparjapati59@gmail.com>
2023-01-17 11:19:36 +0530

    changes to deploy on netlify v2

---
+++ templates/static/javascript/common.js
@@ -0,0 +1,9 @@
+document.getElementById('item1').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com";
+    console.log('hatoikojk');
+    localStorage.clear();
+});
+document.getElementById('item3').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/about";
+    console.log('hatoikojk')
+})
---
+++ templates/static/javascript/index.js
@@ -0,0 +1,21 @@
+// Added click functionality to buttons
+
+document.getElementById('java').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/javaQuiz";
+})
+
+document.getElementById('c').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/cQuiz";
+})
+document.getElementById('sql').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/sqlQuiz";
+})
+document.getElementById('csshtml').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/csshtmlQuiz";
+})
+document.getElementById('javascript').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/javascriptQuiz";
+})
+document.getElementById('python').addEventListener('click', ()=> {
+    location.href="https://quiz-project-harinder.herokuapp.com/pythonQuiz";
+})
